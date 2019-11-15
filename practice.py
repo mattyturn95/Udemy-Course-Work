a03b1ee207e98f365689ea1cc3d7122b9ac41df9
mattyturn95 <mattyturn95@gmail.com>
2019-11-15 14:49:32 +0000

    New lists added

---
+++ practice.py
@@ -0,0 +1,9 @@
+my_list = ["apples", "Bannanas", "Carrots", "Peas"]
+another_list = ["milk", "Bread", "Water", "Beef"]
+
+new_list = my_list + another_list
+
+new_list.append("Oranges")
+new_list.append("Citrus")
+
+print(new_list)
